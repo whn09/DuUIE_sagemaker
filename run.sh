18fffdd1e3d3d08fbd95916e13ca31af60666c5f
EC2 Default User <ec2-user@ip-172-16-181-25.ec2.internal>
2022-06-10 10:33:44 +0000

    add run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
-conda create -n paddlenlp python=3.7
-source activate paddlenlp
-
-pip install =r requirements.txt
+# conda create -n paddlenlp python=3.7
+# source activate paddlenlp
+# pip install =r requirements.txt
 
 python process_data.py preprocess
 
 python3 run_seq2struct.py                              \
-  --multi_task_config config/multi-task-duuie.yaml     \
+  --multi_task_config config/multi-task-duuie-local.yaml     \
   --negative_keep 1.0                                  \
   --do_train                                           \
   --metric_for_best_model=all-task-ave                 \
@@ -18,7 +17,10 @@ python3 run_seq2struct.py                              \
   --logging_dir=output/duuie_multi_task_b32_lr5e-4_log \
   --learning_rate=5e-4                                 \
   --overwrite_output_dir                               \
-  --gradient_accumulation_steps 1
+  --gradient_accumulation_steps 1                      \
+  --device gpu
+
+#   --model_name_or_path=./duuie_multi_task_b32_lr5e-4/ckpt_epoch9                \
 
 python process_data.py split-test
 python inference.py --data data/duuie_test_a --model output/duuie_multi_task_b32_lr5e-4
